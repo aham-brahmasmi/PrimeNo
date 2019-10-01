ebf89f1644e4a63719ec720cbf17847ccabbcd62
aham-brahmasmi <55964282+aham-brahmasmi@users.noreply.github.com>
2019-10-01 19:35:22 +0530

    Main.java

--- HelloWorld/src/Main.java
+++ HelloWorld/src/Main.java
@@ -8,18 +8,18 @@ public class Main{
 	  
 	  if(n==0||n==1)
 	  {  
-	   System.out.println(n+" is not prime number");      
+	   System.out.println(n+" is not the prime number");      
 	  }
 		else
 		{  
 	   for(i=2;i<=m;i++)
 	   {      
 	    if(n%i==0){      
-	     System.out.println(n+" is not prime number");      
+	     System.out.println(n+" is not the prime number");      
 	     flag=1;      
 	     break;      
 	    
 	   }      
-	   if(flag==0)  { System.out.println(n+" is prime number"); }  
+	   if(flag==0)  { System.out.println(n+" is the prime number"); }  
 	  }//end of else  
 } 
